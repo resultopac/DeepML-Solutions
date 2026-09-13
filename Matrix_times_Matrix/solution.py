a0eb5e294cbe89f96e2fe912fd93eb9d8d8d1ffa
resultopac <resultopac04@gmail.com>
2026-09-13 18:53:55 +0300

    Add solution for Matrix times Matrix

---
+++ Matrix_times_Matrix/solution.py
@@ -0,0 +1,12 @@
+import torch
+
+def matrixmul(a, b) -> torch.Tensor:
+    """
+    Multiply two matrices using PyTorch.
+    Inputs can be Python lists, NumPy arrays, or torch Tensors.
+    Returns a 2D tensor of shape (m, n) or a scalar tensor -1 if dimensions mismatch.
+    """
+    return torch.matmul(a, b)
+
+    if a.shape[1] != b.shape[0]:
+        return -1
